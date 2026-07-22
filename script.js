b0ac875df47eb3d1d73433ba962682ecf3624a6a
Sadaf Sadeqeen Chowdhury <s.sadeqeen1212@gmail.com>
2026-07-22 22:13:38 +0600

    add animations and javascript

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
+const words = [
+    "CSE Student",
+    "Competitive Programmer",
+    "Media Officer",
+    "Batch Coordinator"
+];
 
+let wordIndex = 0;
+let charIndex = 0;
+
+const typing = document.getElementById("typing");
+
+function type() {
+    if (charIndex < words[wordIndex].length) {
+        typing.textContent += words[wordIndex][charIndex];
+        charIndex++;
+        setTimeout(type, 100);
+    } else {
+        setTimeout(erase, 1500);
+    }
+}
+
+function erase() {
+    if (charIndex > 0) {
+        typing.textContent =
+            words[wordIndex].substring(0, charIndex - 1);
+
+        charIndex--;
+        setTimeout(erase, 50);
+    } else {
+        wordIndex++;
+
+        if (wordIndex >= words.length) {
+            wordIndex = 0;
+        }
+
+        setTimeout(type, 300);
+    }
+}
+
+type();
